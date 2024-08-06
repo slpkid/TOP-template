2e7fc60e9f024ac89a65f695b5ae10d4809b7bcf
slpkid-laptop <153894144+slpkid@users.noreply.github.com>
2024-08-05 17:23:24 -0700

    does this work..?

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 module.exports = {
   entry: './src/index.js',
   output: {
